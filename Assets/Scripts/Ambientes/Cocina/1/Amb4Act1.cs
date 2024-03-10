9eee13eaf03ca35826070460c1bcccfc76ed443b
Talentum GameDev 13 <143554849+talentumgamedev13@users.noreply.github.com>
2024-03-10 03:43:27 -0500

    Feacture: Experence Scripts de las actividades

---
+++ Assets/Scripts/Ambientes/Cocina/1/Amb4Act1.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Amb4Act1 : ExperienceController
+{
+    
+}
---
+++ Assets/Scripts/Ambientes/Granja/2/Amb3Act2.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Amb3Act2 : ExperienceController
+{
+    
+}
