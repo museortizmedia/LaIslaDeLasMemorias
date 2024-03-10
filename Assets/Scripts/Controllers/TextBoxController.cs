9f53ddd5132aef969a33daaaa4518cc2be4f7d89
Talentum GameDev 13 <143554849+talentumgamedev13@users.noreply.github.com>
2024-03-09 20:43:08 -0500

    Feacture: TextBox Controller
    - Controlador de TextBox para hacer aparecer dialogos

---
+++ Assets/Scripts/Controllers/TextBoxController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+[Serializable]
+public class TextBoxDialog{
+    public string Title;
+    public string Content;
+    public AudioClip Audio;
+}
+
+[RequireComponent(typeof(AudioSource))]
+public class TextBoxController : MonoBehaviour
+{    
+    public List<TextBoxDialog> Dialogs = new List<TextBoxDialog>();
+    public TextMeshProUGUI TitleBox, ContentBox;
+    public Button NextBTN, BackBTN;
+    [SerializeField] int _currentI = 0;
+    public int CurrentI
+    {
+        get { return _currentI; }
+        set
+        {
+            StopAllCoroutines();
+            _currentI = value>=Dialogs.Count?0:value<0?Dialogs.Count:value;
+            NextBTN.gameObject.SetActive(_currentI < Dialogs.Count - 1);
+            BackBTN.gameObject.SetActive(_currentI > 0);
+            SetContentBox();
+        }
+    }
+    public float secondsAfterFinish = 2f;
+    public UnityEvent OnFinish;
+    [SerializeField] AudioSource _as;
+
+    private void Awake() {
+        _as = GetComponent<AudioSource>();
+        _as.playOnAwake = false;
+    }
+
+    private void Start()
+    {
+        NextBTN.onClick.AddListener(GoNext);
+        BackBTN.onClick.AddListener(GoBack);
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.RightArrow)){
+            GoNext();
+        }else if(Input.GetKeyDown(KeyCode.LeftArrow)){
+            GoBack();
+        }
+    }
+
+    private void OnEnable() {
+        CurrentI = 0;
+    }
+
+    public void GoNext(){
+        CurrentI++;
+    }
+    public void GoBack(){
+        CurrentI--;
+    }
+    void SetContentBox(){
+        TitleBox.text = Dialogs[CurrentI].Title;
+        ContentBox.text = Dialogs[CurrentI].Content;
+        _as.clip = Dialogs[CurrentI].Audio;
+        if(_as.clip!=null){
+            _as.Play();
+            if(_currentI==Dialogs.Count-1)
+            {
+                StartCoroutine(nameof(WaitDialog));
+            }
+        }
+    }
+
+    IEnumerator WaitDialog(){
+        yield return new WaitForSeconds(_as.clip.length+secondsAfterFinish);
+        OnFinish?.Invoke();
+    }
+}
