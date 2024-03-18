ecfae464fafee8fec8d85447525898c034929ecd
Gabriela Peralta <64208255+solaridemon@users.noreply.github.com>
2024-03-17 22:23:03 -0500

    Feat: Info Slide3-Slide4 HU1.3.3
    Logica para que muestre la información en Slide 4 según la actividad y escenario seleccionado en Slide 3

--- Assets/Scripts/Controllers/DificultMeter.cs
+++ Assets/Scripts/Controllers/DificultMeter.cs
@@ -1,26 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor;
+// using UnityEditor;
+using TMPro;
 
 public class DificultMeter : MonoBehaviour
 {
     [SerializeField] Sprite empty, full;
+    [SerializeField] TextMeshProUGUI label;
     public int Levels = 3;
-    int _dificult;
+    [Range(0,2)][SerializeField]  int _dificult = 0;
     public int Dificult{
         get => _dificult;
         set {
+
+            if (prev.Length == 0)
+            {
+                prev = gameObject.GetComponentsInChildren<Image>(true);
+            }
+
             _dificult = value;
-            if(prev!=null){prev.sprite = empty;}
-            prev = transform.GetChild(value).GetComponent<Image>();
-            prev.sprite = full;
+            label.text = _dificult == 0 ? "Fácil" : _dificult == 1 ? "Normal" : "Difícil";
+
+            for (int i = 0; i < prev.Length; i++)
+            {
+                prev[i].sprite = i<=_dificult? full:empty;
+            }
         }
     }
-    Image prev;
+    Image[] prev = new Image[]{};
 }
-
+/*
 [CustomEditor(typeof(DificultMeter))]
 public class MyComponentEditor : Editor
 {
@@ -35,3 +44,4 @@ public class MyComponentEditor : Editor
         myComponent.Dificult = EditorGUILayout.IntSlider("Dificult", myComponent.Dificult, 0, myComponent.Levels-1);
     }
 }
+*/
--- Assets/Scripts/Controllers/DropDownController.cs
+++ Assets/Scripts/Controllers/DropDownController.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DropDownController : MonoBehaviour
 {
     public TMP_Dropdown DropAmbiente, DropActividad;
     public TextMeshProUGUI Description;
     public ScriptableActivitiesInfo GameInfo;
+
+    [Space(20)]
+    [Header("Slide4")]
+    public TextMeshProUGUI NombreActividad, NombrePersonaje, ObjetivoActividad, TiempoActividad;
+    public DificultMeter Medidor;
+    public Image Personaje;
 
     private void Start() {
         GameInfo = Resources.Load<ScriptableActivitiesInfo>("Scriptables/ActivitiesInfoScriptable");
@@ -36,6 +43,20 @@ public class DropDownController : MonoBehaviour
     public void SetManagerconfig(){
         Managers.Instance.AmbienteActual = DropAmbiente.value;
         Managers.Instance.ActividadActual = DropActividad.value;
+        ScriptableActivitiesInfo.Ambiente.Actividad ConfiguracionActividad = GameInfo.Ambientes[DropAmbiente.value]._activitiesNames[DropActividad.value];
+        SetCharacter(ConfiguracionActividad);
+    }
+
+
+    void SetCharacter(ScriptableActivitiesInfo.Ambiente.Actividad ConfiguracionActividad)
+    {
+        NombreActividad.text = ConfiguracionActividad._name;
+        NombrePersonaje.text = ConfiguracionActividad._nombrePersonaje;
+        ObjetivoActividad.text = ConfiguracionActividad._desc;
+        TiempoActividad.text = (ConfiguracionActividad._tiempoEstimado/60).ToString()+" Minutos";
+        Medidor.Dificult = (int)ConfiguracionActividad._dificultad;
+        Personaje.sprite = ConfiguracionActividad._personaje;
+
     }
 
     public void ChangeToInicio(){
