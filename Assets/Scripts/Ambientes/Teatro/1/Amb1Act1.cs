095ba3ed3705b9d24c4473a31984a26cbfb38a29
Talentum GameDev 13 <143554849+talentumgamedev13@users.noreply.github.com>
2024-04-07 03:13:44 -0500

    Fix: Ajustes funcionamiento Teatro

--- Assets/Scripts/Ambientes/Teatro/1/Amb1Act1.cs
+++ Assets/Scripts/Ambientes/Teatro/1/Amb1Act1.cs
@@ -3,53 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Amb1Act1 : ExperienceController
 {
     public List<int> buttonsUsedIndex = new List<int>();
     public TutorialCardController TableController;
 
+    public UnityEvent OnEndTutorial;
+
     public override void Start()
     {
         base.Start(); //llamada obligatoria al comenzar
-        Debug.Log(Manager.GetManager<InputManager>().name);
-    }
-
-    public void ContenidoDePrueba(){
-        InvokeRepeating(nameof(LogicaDePrueba), 0, 1);
-    }
-    int segundos;
-    void LogicaDePrueba()
-    {
-        segundos++;
-        if (segundos >= 5)
-        {
-            CancelInvoke();
-            Debug.Log("Han pasado " + segundos + " segundos.");
-            return;
-        }
-        Debug.Log("Han pasado " + segundos + " segundos.");
+        //Debug.Log(Manager.GetManager<InputManager>().name);
+        IniciarCon(2);
+        OnEndTutorial.AddListener(()=>Finalizar());
     }
 
     void Finalizar()
     {
         Debug.Log("Terminó la experiencia con éxito");
         // Desactiva esta línea para evitar que se cambie la escena
-        // EndExperience(); // llamada obligatoria al finalizar
-        ActivarWorkSpace(); // Llama a un método que activa el WorkSpace
-    }
-
-    void ActivarWorkSpace()
-    {
-        GameObject workSpace = GameObject.Find("WorkSpace"); // Encuentra el GameObject del Canvas
-        if (workSpace != null)
-        {
-            workSpace.SetActive(true);
-        }
-        else
-        {
-            Debug.LogError("No se encontró el Canvas 'WorkSpace'");
-        }
+        EndExperience(); // llamada obligatoria al finalizar
     }
 
     public void NextButton()
@@ -65,7 +40,7 @@ public class Amb1Act1 : ExperienceController
 
         if (availableIndexes.Count == 0)
         {
-            Finalizar();
+            OnEndTutorial?.Invoke();
             return;
         }
 
@@ -74,12 +49,6 @@ public class Amb1Act1 : ExperienceController
 
         buttonsUsedIndex.Add(selectedIndex);
         TableController.ShowInfo(selectedIndex, "Selecciona el botón que se indica en la imagen para realizar otro trazado");
+        Manager.GetManager<InteractableManager>().ChangeInteractionMode(true); //forzar la activación de la interacción
     }
-
-    // TODO Delete this function when the InteractableArea Call the Events
-    public void Update()
-    {
-
-    }
-
 }
