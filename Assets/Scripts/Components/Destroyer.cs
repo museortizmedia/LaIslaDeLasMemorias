575d4c3799804251e59b9661f972cb0dc3edf033
Talentum GameDev 13 <143554849+talentumgamedev13@users.noreply.github.com>
2024-04-20 02:58:56 -0500

    Demo1 update

--- Assets/Scripts/Components/Destroyer.cs
+++ Assets/Scripts/Components/Destroyer.cs
@@ -6,19 +6,22 @@ using UnityEngine.Events;
 public class Destroyer : MonoBehaviour
 {
     public GameObject Target;
+    public Component Component;
     public float SecondToDestroy=0;
     public UnityEvent BeforeDestroy, AfterDestroy;
     public void DestroyIt(){
         if(Target!=null){
-            StartCoroutine(Destroying(Target));
+            StartCoroutine(nameof(Destroying));
         }else{
-            StartCoroutine(Destroying(gameObject));
+            StartCoroutine(nameof(Destroying));
         }
     }
-    IEnumerator Destroying(GameObject eliminateGO){
+    IEnumerator Destroying(){
         BeforeDestroy?.Invoke();
         yield return new WaitForSeconds(SecondToDestroy);
         AfterDestroy?.Invoke();
-        Destroy(eliminateGO);
+        if(Target!=null){Destroy(Target);} else
+        if(Component!=null){Destroy(Component);} else
+        Destroy(gameObject);
     }
 }
